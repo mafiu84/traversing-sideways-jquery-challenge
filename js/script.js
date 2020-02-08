973ffdd1e0181b773d975f17c6a3dbc51ccfdb52
Mateusz Poplewski <matheush84@gmail.com>
2020-02-08 11:47:25 +0000

    Other Traversing Methods - Challenge 3, using variable to store data and later use this stored data for something else

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ $(document).ready(function(){
     $("button").click(function(){
         $(this).next("p").slideToggle("slow");
     });
+
+    //will display rgb value of selected panel in the reset panel
+    $(".theButton").click(function(){
+    var col = $(this).css('background-color');
+    $('.superButton').text( col);
+    });
+
 });
